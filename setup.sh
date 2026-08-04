d02dc42c622ed6952402062d7e0470b04e1006fd
MisterFOURXXX <fourapiwit@gmail.com>
2026-08-04 15:07:36 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,10 +7,12 @@ git clone https://github.com/hotpotqa/hotpot.git datasets/hotpotqa || echo "Hotp
 echo "Cloning LongMemEval dataset..."
 git clone https://github.com/salesforce/LongMemEval.git datasets/longmemeval || echo "LongMemEval already exists."
 
-echo "Downloading HotpotQA fullwiki data (if not present)..."
-cd datasets/hotpotqa
-wget -nc http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_train_v1.1.json
-wget -nc http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_dev_fullwiki_v1.json
-cd ../..
-
 echo "Dataset setup complete."
+
+# PyTorch with CUDA
+pip install torch==2.9.0 torchvision==0.24.0 torchaudio==2.9.0 --index-url https://download.pytorch.org/whl/cu128
+
+# Python packages
+pip install -r requirements.txt
+
+echo "Environment setup complete!"
